cde32b1d89b3219c65644bd6c8e3dc6fe3eb5897
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-04-25 11:35:47 +0300

    first commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.write("CMT Timesheets Extended")
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
-@st.cache_resource
+# @st.cache_resource
 def init_connection():
     return mysql.connector.connect(**st.secrets["mysql"])
 
