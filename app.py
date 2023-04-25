2d9876fc5f00ba84d26b03319f5d9f7cff4c0e5e
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-04-25 10:27:22 +0300

    first commit

--- app.py
+++ app.py
@@ -3,3 +3,11 @@ import pandas as pd
 import mysql.connector
 
 st.write("CMT Timesheets Extended")
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
